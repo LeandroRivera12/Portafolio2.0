473084f6e19a6f8939147250d46c669e932d0a6b
Leandro Rivera <riveraleandro518@gmail.com>
2024-06-06 23:02:06 -0600

    change today

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import 'animate.css'
 import './Inicio.css'
+import animationText from '../../utils/animationText'
+
 
 const Inicio = () => {
 
+    const textRef = useRef(null)
+
+    useEffect(() => {
+       if (textRef.current) {
+         animationText(textRef.current);
+       }
+      
+    }, [])
     
+
   return (
      <div className='home__container'>
        <div className='home__description'>
             <h1 className='home__header'>Hola, soy</h1>
-            <div className="text__animation"></div>
+            <div className="text__animation" ref={textRef}></div>
             <div className='barra__escritura'></div>    
         </div>
         <div className='img-conatiner'>
--- src/utils/animationText.js
+++ src/utils/animationText.js
@@ -1,5 +1,49 @@
-function animacion () {
-    let textoAnimacion = [
-        
-    ]
+
+const animationText = () => {
+    let textAnimation = [
+        ['L', 'e', 'a', 'n', 'd', 'r', 'o'],
+        ['D', 'e', 's', 'a', 'r', 'r', 'o', 'l', 'l', 'a', 'd', 'o', 'r', ' ', 'W', 'e', 'b']
+    ];
+
+    let countLetter = -1;
+    let levelArray = 0;
+
+    const animationContainer = document.querySelector('.text__animation');
+    function printText(){
+        countLetter++;
+        animationContainer.textContent += textAnimation[levelArray][countLetter];
+
+        if (countLetter === textAnimation[levelArray].length -1) {
+          clearInterval(interval) 
+          setTimeout(() => {
+               interval = setInterval(() => {
+                    animationContainer.textContent = "";
+                    countLetter--;
+                    textAnimation[levelArray].pop();
+
+                    textAnimation[levelArray].forEach(element => {
+                        animationContainer.textContent += element;
+                    });
+
+                    if (countLetter < 0) {
+                        clearInterval(interval);
+                        levelArray++;
+                        interval = setInterval(printText, 150)
+            
+                        if (levelArray > textAnimation.length - 1) {
+                            clearInterval(interval);
+                            levelArray = 0;
+                            animationText();
+                        }
+                    }
+                }, 150);
+          }, 1000);
+        }
+    }
+    let interval = setInterval(printText, 150)
+
+  return window.addEventListener("load", animationText);
+      
 }
+
+export default animationText
